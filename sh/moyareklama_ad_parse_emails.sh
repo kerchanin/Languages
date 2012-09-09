7b1e002d38f12f2ddb5a7cdd71466b2038bbb7ca
Sergey <5h4r3dm3m0ry@gmail.com>
2012-09-09 13:17:18 +0400

    Update sh/moyareklama_ad_parse_emails.sh

--- sh/moyareklama_ad_parse_emails.sh
+++ sh/moyareklama_ad_parse_emails.sh
@@ -8,13 +8,13 @@ startpage="http://www.moyareklama.ru/${city}/работа_вакансии/вс�
 user_agent='Mozilla/5.0 (X11; Linux i686; rv:17.0) Gecko/17.0 Firefox/17.0'
 
 #5131
-found_quantity=`wget -q -O - -U $user_agent "http://www.moyareklama.ru/${city}/работа_вакансии/все" | \
+found_quantity=`wget -q -O - -U "$user_agent" "http://www.moyareklama.ru/${city}/работа_вакансии/все" | \
   grep -i 'Найдено' | \
     grep -o -E '[0-9]+'`
 echo $found_quantity
 
 #/работа_вакансии/все/8/343
-last_relative_url=`wget -q -O - -U 'Mozilla' "http://www.moyareklama.ru/${city}/работа_вакансии/все" | \
+last_relative_url=`wget -q -O - -U "$user_agent" "http://www.moyareklama.ru/${city}/работа_вакансии/все" | \
   sed 's/<\/a>/<\/a>\n/g' | \
     grep "<a class='page_m' href=\"" | \
       grep -v strelka | \
@@ -36,7 +36,7 @@ absolute_url_without_id=${host}${relative_url_without_id}
 
 #!
 cookie_file=cookies_`date +'%Y.%m.%d.%s'`.txt
-wget --save-cookies=$cookie_file -U $user_agent --spider $host/${city}/
+wget --save-cookies=$cookie_file -U "$user_agent" --spider $host/${city}/
 
 #ids_2012.09.09.1347173733.txt
 id_file=ids_`date +'%Y.%m.%d.%s'`.txt
@@ -48,7 +48,7 @@ id_file=ids_`date +'%Y.%m.%d.%s'`.txt
 for id in `seq 1 $last_number` ; do
     url=${absolute_url_without_id}$id
     #/single_ad_new.php?id=
-    wget --load-cookies=$cookie_file -U $user_agent $url -O - -q | \
+    wget --load-cookies=$cookie_file -U "$user_agent" $url -O - -q | \
       #grep --color=auto -o -E '/single_ad_new\.php\?id=[0-9]{1,15}' | tee -a $id_file
       grep --color=auto -o -E '/single_ad_new\.php\?id=[0-9]{1,15}' >> $id_file
 done
@@ -60,7 +60,7 @@ sed -i 's/^/http:\/\/www.moyareklama.ru/g' $id_file
 email_file=emails_`date +'%Y.%m.%d.%s'`.txt
 
 for url in `cat $id_file` ; do
-    wget --load-cookies=$cookie_file -U $user_agent $url -O - -q | \
+    wget --load-cookies=$cookie_file -U "$user_agent" $url -O - -q | \
       grep --color=auto -o -i -E '\b[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,10}\b' | sort -u | tee -a $email_file
 done
 
