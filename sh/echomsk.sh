63cd20b16409b4990121d877770d32f289e9481e
5h4r3dm3m0ry@gmail.com <5h4r3dm3m0ry@gmail.com>
2013-06-11 19:42:46 +0400

    Languages

--- sh/echomsk.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-export SDL_VIDEODRIVER=dummy
-ffplay 'http://radio.memonet.ru:8000/echo.mp3' > /dev/null 2>&1 &
--- sh/europaplus.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-bitrate=32
-# 32 64 128
-[ $1 ] && bitrate=$1
-export SDL_VIDEODRIVER=dummy
-ffplay http://webcast.emg.fm:55655/europaplus$bitrate.mp3 > /dev/null 2>&1 &
-
